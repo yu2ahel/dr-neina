7083d7a298e14e1e41c0463931667d5559bd5040
Hazem Safwat <HSafwat@moj.gov.sa>
2023-06-18 03:27:55 +0300

    adding registration api

---
+++ app/Http/Controllers/AiBot/BotController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\AiBot;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+        $token = $user->createToken('myAppToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
